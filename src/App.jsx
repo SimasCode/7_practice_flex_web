1b7057c8d23eef05952805c3039dd47a97da38e6
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-14 11:44:41 +0300

    nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import GainMore from './components/gainMore/GainMore';
+import NavBar from './components/navbar/NavBar';
 import './style/App.css';
 import './style/reset.css';
 
 export default function App() {
   return (
     <div className='main-container'>
+      <NavBar />
       <GainMore />
     </div>
   );
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,15 +1,31 @@
 export default function NavBar() {
   return (
     <div className='nav-container'>
-      <a href="#"><img src="" alt="logo" /></a>
-      <div className="nav-text-container">
-        <a href='#'>Product</a>
-        <a href='#'>Features</a>
-        <a href='#'>Pricing</a>
-        <a href='#'>Product</a>
+      <a href='#'>
+        <img src='/images/Logo.png' alt='logo' />
+      </a>
+      <div className='nav-text-container'>
+        <a className='nav-text' href='#'>
+          Product
+        </a>
+        <a className='nav-text' href='#'>
+          Features
+        </a>
+        <a className='nav-text' href='#'>
+          Pricing
+        </a>
+        <a className='nav-text' href='#'>
+          Product
+        </a>
+      </div>
+      <div className='nav-login-container'>
+        <a className='nav-text' href='#'>
+          Log In
+        </a>
+        <a className='nav-btn' href='#'>
+          Sign Up
+        </a>
       </div>
     </div>
   );
 }
-
-export NavBar
